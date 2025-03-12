2781bd12e09e7ea4e278900ed49f1a1e5df6ba30
Mitch3lle <wakwayamitchelle@gmail.com>
2025-03-12 07:29:58 +0300

    added ai API intergration and axios

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
+  function getPoem(response){
+    console.log("Poem Generated");
+    new Typewriter("#poem", {
+      strings:response.data.answer,
+      autoStart: true,
+      delay:1,
+      cursor:"",
+      
+    });
+  }
+
+  
+  
+  
+  
   function generatePoem(event){
     event.preventDefault();
-
-   
-   new Typewriter("#poem", {
-    strings:"Poem will go here...",
-    autoStart: true,
-    delay:1,
-    cursor:"",
     
-  });
+    let instructionsInput = document.querySelector("#user-instructions");
+
+
+    let apiKey = "ef080d077be87f0o3fbae14a3eab95ta";
+    let Prompt =`Users Instructions: You are a talented poet known for your creativity and vivid imagery. Write a beautiful 4-line poem based. Make it meaningful, engaging, and poetic ${instructionsInput.value} `;
+    let context = "You are an expert poet, highly creative and expressive. Generate a 4-line poem based on the User's Instructions. Format the poem using HTML by wrapping each line in `<br>`. At the end, sign it with 'Waks Chatbox' in bold using `<b>`.";
+
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${Prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating Poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(getPoem);
    
+
   }         
 let poemformElement = document.querySelector("#poem-generator-form");
 poemformElement.addEventListener("submit", generatePoem);
