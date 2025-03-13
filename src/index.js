898ec3a3915dfc113a232d2a8d96167af6c112d0
Mitch3lle <wakwayamitchelle@gmail.com>
2025-03-13 07:19:33 +0300

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
     new Typewriter("#poem", {
       strings:response.data.answer,
       autoStart: true,
-      delay:1,
+      delay:50,
       cursor:"",
       
     });
@@ -21,9 +21,15 @@
 
     let apiKey = "ef080d077be87f0o3fbae14a3eab95ta";
     let Prompt =`Users Instructions: You are a talented poet known for your creativity and vivid imagery. Write a beautiful 4-line poem based. Make it meaningful, engaging, and poetic ${instructionsInput.value} `;
-    let context = "You are an expert poet, highly creative and expressive. Generate a 4-line poem based on the User's Instructions. Format the poem using HTML by wrapping each line in `<br>`. At the end, sign it with 'Waks Chatbox' in bold using `<b>`.";
+    let context = "You are an expert poet, highly creative and expressive. Generate a 4-line poem based on the User's Instructions. Format the poem using HTML by wrapping each line in `<br>`. At the end, sign it with 'Waks Chatbox ✨' in bold using `<b>`.";
 
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${Prompt}&context=${context}&key=${apiKey}`;
+    
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = ` <div class = "generating" > ⏳Generating poem about ${instructionsInput.value}...`;
+
+
 
     console.log("Generating Poem");
     console.log(`Prompt: ${prompt}`);
